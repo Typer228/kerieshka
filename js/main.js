ba0b0d10b59743dd18e1ad77257265e26f733f1e
local <121034098+Typer228@users.noreply.github.com>
2024-07-14 21:17:08 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Telegram.WebApp.ready();
 const user = Telegram.WebApp.initDataUnsafe.user;
 const userId = user.id;
 const userName = user.username;
-const userPhotoUrl = user.photo_url;
+let userPhotoUrl = user.photo_url || 'photo/standard_foto.png';
 
 fetch('/register', {
     method: 'POST',
@@ -18,6 +18,8 @@ fetch('/register', {
 .then(data => {
     // Обновление баланса
     document.getElementById('userBalance').innerText = `Баланс: ${data.balance}`;
+    userPhotoUrl = data.photo_url || 'photo/standard_foto.png';
+    document.querySelector('.avatar').src = userPhotoUrl;
 })
 .catch(error => console.error('Ошибка:', error));
 
@@ -25,6 +27,7 @@ const profileContent = `
   <div class="profile">
     <img src="${userPhotoUrl}" alt="Avatar" class="avatar">
     <h2>${userName}</h2>
+    <button class="change-photo-btn">Изменить фотку</button>
   </div>
 `;
 
@@ -34,6 +37,47 @@ const infoContent = '<h6>Информация</h6>';
 function changeContent(content) {
     const mainContent = document.getElementById('mainContent');
     mainContent.innerHTML = content;
+    if (content.includes('change-photo-btn')) {
+        document.querySelector('.change-photo-btn').addEventListener('click', openPhotoUrlModal);
+    }
+}
+
+function openPhotoUrlModal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = `
+        <div class="modal-content">
+            <span class="close-btn">&times;</span>
+            <h2>Введите URL новой фотографии</h2>
+            <input type="text" id="photoUrlInput" placeholder="URL фотографии">
+            <button id="savePhotoUrlBtn">Сохранить</button>
+        </div>
+    `;
+    document.body.appendChild(modal);
+
+    document.querySelector('.close-btn').addEventListener('click', () => {
+        document.body.removeChild(modal);
+    });
+
+    document.getElementById('savePhotoUrlBtn').addEventListener('click', () => {
+        const newPhotoUrl = document.getElementById('photoUrlInput').value;
+        if (newPhotoUrl) {
+            fetch('/update-photo', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: userId, photo_url: newPhotoUrl })
+              })
+            .then(response => response.json())
+            .then(data => {
+                userPhotoUrl = data.photo_url || 'photo/standard_foto.png';
+                document.querySelector('.avatar').src = userPhotoUrl;
+                document.body.removeChild(modal);
+            })
+            .catch(error => console.error('Ошибка:', error));
+        }
+    });
 }
 
 document.getElementById('marketBtn').addEventListener('click', () => changeContent(marketContent));
